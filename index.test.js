0e129c57e002c7d76f29ba72390df7106769e71d
Tino Vyatkin <tino@vtkn.io>
2019-06-03 21:41:49 -0400

    upgrade all deps and lint

--- index.test.js
+++ index.test.js
@@ -5,15 +5,15 @@ const stylelint = require("stylelint");
 const { join } = require("path");
 const { readFileSync } = require("fs");
 
-function lint(filename, warningsCount) {
-  return stylelint.lint({
+async function lint(filename, warningsCount) {
+  const result = await stylelint.lint({
     code: readFileSync(join(__dirname, "css", `${filename}.css`), "utf8"),
     config,
-  })
-  .then(result => {
-    const { results:  [{ warnings }] } = result
-    expect(warnings).toHaveLength(warningsCount)
-  })
+  });
+
+  const { results:  [{ warnings }] } = result
+
+  expect(warnings).toHaveLength(warningsCount)
 }
 
 it("results in no warnings on valid CSS", () => lint("valid", 0))
