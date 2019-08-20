dd1b34808db4bde4da169bad2cd81e0be2338c34
Tino Vyatkin <tino@vtkn.io>
2019-08-20 10:16:29 -0400

    allow universal selector, whitelist atRules

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 'use strict';
 
 module.exports = {
-  "rules": {
-    "declaration-no-important": true,
-    "keyframe-declaration-no-important": true,
-    "selector-max-universal": 0,
-    "selector-pseudo-class-blacklist": "not",
-    "selector-max-type": [ 0, { ignoreTypes: "/^((?!^i-amp-).)*$/" } ],
-    "selector-class-pattern": "^((?!^-amp-).)*$",
-    "property-blacklist": [ "behavior", "-moz-binding", "filter" ],
-    "declaration-property-value-whitelist": {
-      "transition": [ "/opacity/", "/transform/" ],
+  rules: {
+    'declaration-no-important': true,
+    'keyframe-declaration-no-important': true,
+    'selector-max-type': [0, { ignoreTypes: '/^((?!^i-amp-).)*$/' }],
+    'selector-class-pattern': '^((?!^-amp-).)*$',
+    'selector-id-pattern': '^((?!^-amp-).)*$',
+    'property-blacklist': ['behavior', '-moz-binding', 'filter'],
+    'declaration-property-value-whitelist': {
+      transition: ['/opacity/', '/transform/']
     },
-  },
-}
+    'at-rule-whitelist': [
+      '@font-face',
+      '@keyframes',
+      '@media',
+      '@page',
+      '@supports'
+    ]
+  }
+};
--- index.test.js
+++ index.test.js
@@ -1,21 +1,23 @@
 'use strict';
 
-const config = require("./");
-const stylelint = require("stylelint");
-const { join } = require("path");
-const { readFileSync } = require("fs");
+const config = require('./');
+const stylelint = require('stylelint');
+const { join } = require('path');
+const { readFileSync } = require('fs');
 
 async function lint(filename, warningsCount) {
   const result = await stylelint.lint({
-    code: readFileSync(join(__dirname, "css", `${filename}.css`), "utf8"),
-    config,
+    code: readFileSync(join(__dirname, 'css', `${filename}.css`), 'utf8'),
+    config
   });
 
-  const { results:  [{ warnings }] } = result
+  const {
+    results: [{ warnings }]
+  } = result;
 
-  expect(warnings).toHaveLength(warningsCount)
+  expect(warnings).toHaveLength(warningsCount);
 }
 
-it("results in no warnings on valid CSS", () => lint("valid", 0))
+it('results in no warnings on valid CSS', () => lint('valid', 0));
 
-it("results in 11 warnings on invalid CSS", () => lint("invalid", 11))
+it('results in 11 warnings on invalid CSS', () => lint('invalid', 11));
